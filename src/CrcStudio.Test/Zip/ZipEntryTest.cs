b613d6d97f32222d65979a4cb607ce851a903686
jens13@gmail.com <jens13@gmail.com@4a942f15-36e7-dbdf-8c75-66494e521ec5>
2011-12-11 22:05:43 +0000

    updated ZipEntryTest
    git-svn-id: https://custom-rom-creator-studio.googlecode.com/svn/trunk@86 4a942f15-36e7-dbdf-8c75-66494e521ec5

--- src/CrcStudio.Test/Zip/ZipEntryTest.cs
+++ src/CrcStudio.Test/Zip/ZipEntryTest.cs
@@ -21,10 +21,35 @@ namespace CrcStudio.Test.Zip
         [TestMethod]
         public void CompressionTypeTest()
         {
-            var zf = new ZipFile("test.zip");
-            zf.Add(new MemoryStream(new byte[0]), "ZeroBytes", CompressionType.Deflate);
-            zf.Add(new MemoryStream(Encoding.ASCII.GetBytes("test")), "Test", CompressionType.Deflate);
-            zf.Close();
+            File.Delete("test.zip");
+            using (var zf = new ZipFile("test.zip"))
+            {
+                zf.Add(new MemoryStream(new byte[0]), "ZeroBytes", CompressionType.Deflate);
+                zf.Add(new MemoryStream(Encoding.ASCII.GetBytes("test")), "Test", CompressionType.Deflate);
+                var test = "test";
+                for (int i = 0; i < 1000; i++) test += "test";
+                zf.Add(new MemoryStream(Encoding.ASCII.GetBytes(test)), "LargeTest", CompressionType.Deflate);
+            }
+            using (var zf = new ZipFile("test.zip"))
+            {
+                var zipEntry = zf.Entries.ToArray()[0];
+                Assert.AreEqual(zipEntry.CompressionMethod, (ushort)CompressionType.Store);
+                Assert.AreEqual((int)zipEntry.CompressedSize, 0);
+                Assert.AreEqual((int)zipEntry.UncompressedSize, 0);
+
+                for (int n = 1; n < 3; n++)
+                {
+                    zipEntry = zf.Entries.ToArray()[n];
+                    if (zipEntry.CompressedSize == zipEntry.UncompressedSize)
+                    {
+                        Assert.AreEqual(zipEntry.CompressionMethod, (ushort) CompressionType.Store);
+                    }
+                    else
+                    {
+                        Assert.AreEqual(zipEntry.CompressionMethod, (ushort) CompressionType.Deflate);
+                    }
+                }
+            }
         }
     }
 }
