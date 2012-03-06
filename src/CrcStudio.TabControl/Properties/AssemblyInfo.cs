e0f27680ea52aac5273d1c3751a2ecb82deb9d52
jens13@gmail.com <jens13@gmail.com@4a942f15-36e7-dbdf-8c75-66494e521ec5>
2012-03-06 21:34:37 +0000

    Release 1.0.0.0 Changed version number in application
    git-svn-id: https://custom-rom-creator-studio.googlecode.com/svn/trunk@95 4a942f15-36e7-dbdf-8c75-66494e521ec5

--- src/CrcStudio.TabControl/Properties/AssemblyInfo.cs
+++ src/CrcStudio.TabControl/Properties/AssemblyInfo.cs
@@ -41,5 +41,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.5.2")]
+[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
